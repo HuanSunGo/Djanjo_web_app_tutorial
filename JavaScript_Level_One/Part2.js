115387144626e844fd9559a131e6f417fd2d9572
Huan Sun <hs3240@columbia.edu>
2023-07-14 11:26:09 -0400

    add all files

---
+++ JavaScript_Level_One/Part2.js
@@ -0,0 +1,7 @@
+alert("Welcome to your bank's website!")
+// promot is the window pop up at the page, similar to input in python 
+var deposit = prompt("How much would you like to deposit today?")
+alert("You've deposited: "+deposit+" into your account.")
+
+// console.log to print the result in the console(see it in inspect)
+console.log("You are a cool person!")
---
+++ JavaScript_Level_One/mySolution.js
@@ -0,0 +1,3 @@
+var pounds = prompt("Input the pound that you want to convert")
+var kilo = pounds * 0.454 
+alert(pounds + "pounds is " +  kilo + "kilo" )
